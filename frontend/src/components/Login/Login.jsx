79a9db29910564755b2576a077d662af034b0f91
ys-333 <yashwantsing333@gmail.com>
2023-12-16 00:33:18 +0530

    handling state of login

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,67 @@
 import styles from './Login.module.css'
+
+import { useReducer } from 'react'
+
+// for state management we would be using useReducer
+
+const initalState = {
+  name: 'xyz',
+  email: 'xyz@gmail.com',
+  password: 'xyz',
+  location: 'xyz',
+}
+
+function reducerFn(state, action) {
+  switch (action.type) {
+    case 'name': {
+      return {
+        ...state,
+        name: action.value,
+      }
+    }
+    case 'email': {
+      return {
+        ...state,
+        name: action.value,
+      }
+    }
+    case 'password': {
+      return {
+        ...state,
+        name: action.value,
+      }
+    }
+    case 'location':
+      {
+        return {
+          ...state,
+          name: action.value,
+        }
+      }
+      throw Error('Undefined Acion type' + action.type)
+  }
+}
+
 const Login = () => {
+  const [state, dispatch] = useReducer()
+
+  function changeHandler(type, value) {
+    switch (type) {
+      case 'name':
+        dispatch({ type: 'name', value })
+      case 'email':
+        dispatch({ type: 'email', value })
+      case 'password':
+        dispatch({ type: 'password', value })
+      case 'location':
+        dispatch({ type: 'location', value })
+    }
+  }
+
+  const submitHandler = () => {
+    // ? submit handler
+  }
+
   return (
     <form className={styles.form}>
       <div className={styles.container}>
@@ -53,3 +115,15 @@ const Login = () => {
   )
 }
 export default Login
+
+/*
+
+        
+/welcome page
+    -> Show all page (if authenticated) 
+            ->redirect to <Post/> components
+    if(not authenticated)   
+        -> redirect to Login/Signup
+
+
+*/
