dd6ba1a148b4071716fd81ab38d0ab2c5c152d2c
ys-333 <yashwantsing333@gmail.com>
2023-12-16 02:19:23 +0530

    login page completed

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import styles from './Login.module.css'
 
 import { useReducer } from 'react'
+import { useState } from 'react'
 
 // for state management we would be using useReducer
 
@@ -44,6 +45,7 @@ function reducerFn(state, action) {
 
 const Login = () => {
   const [state, dispatch] = useReducer()
+  const [isLogin, setIsLogin] = useState(true)
 
   function changeHandler(type, value) {
     switch (type) {
@@ -58,60 +60,93 @@ const Login = () => {
     }
   }
 
+  function loginChangeHandler() {
+    setIsLogin((prev) => !prev)
+  }
+
   const submitHandler = () => {
     // ? submit handler
   }
 
   return (
-    <form className={styles.form}>
-      <div className={styles.container}>
-        <label className={styles.label} htmlFor="name">
-          Name
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          id="name"
-          placeholder="Full Name.."
-        />
-      </div>
-      <div className={styles.container}>
-        <label className={styles.label} htmlFor="email">
-          Email
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          id="email"
-          placeholder="email"
-        />
-      </div>
-      <div className={styles.container}>
-        <label className={styles.label} htmlFor="password">
-          Password
-        </label>
-        <input
-          className={styles.input}
-          type="password"
-          id="password"
-          placeholder="password"
-        />
-      </div>
-      <div className={styles.container}>
-        <label className={styles.label} htmlFor="Location">
-          Location
-        </label>
-        <input
-          className={styles.input}
-          type="text"
-          id="Location"
-          placeholder="location"
-        />
-      </div>
-      <div className={styles.container}>
-        <input type="submit" value="Submit" className={styles.submit} />
-      </div>
-    </form>
+    <div className={styles.form}>
+      <form>
+        {!isLogin && (
+          <div className={styles.container}>
+            <label className={styles.label} htmlFor="name">
+              Name
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              id="name"
+              placeholder="Full Name.."
+            />
+          </div>
+        )}
+        <div className={styles.container}>
+          <label className={styles.label} htmlFor="email">
+            Email
+          </label>
+          <input
+            className={styles.input}
+            type="text"
+            id="email"
+            placeholder="email"
+          />
+        </div>
+        <div className={styles.container}>
+          <label className={styles.label} htmlFor="password">
+            Password
+          </label>
+          <input
+            className={styles.input}
+            type="password"
+            id="password"
+            placeholder="password"
+          />
+        </div>
+        {!isLogin && (
+          <div className={styles.container}>
+            <label className={styles.label} htmlFor="Location">
+              Location
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              id="Location"
+              placeholder="location"
+            />
+          </div>
+        )}
+        <div className={styles.container}>
+          <input
+            type="submit"
+            value={isLogin ? 'Login' : 'Signup'}
+            className={styles.submit}
+          />
+        </div>
+      </form>
+      <p>
+        {isLogin ? (
+          <button
+            onClick={() => {
+              setIsLogin((prev) => !prev)
+            }}
+          >
+            Create Account? Sign up
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setIsLogin((prev) => !prev)
+            }}
+          >
+            Already User? Login
+          </button>
+        )}
+      </p>
+    </div>
   )
 }
 export default Login
